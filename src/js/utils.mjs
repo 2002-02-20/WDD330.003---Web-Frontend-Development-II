8d45fff876dfe5deca6bfcb4fb583d1c9b6b61e2
dereklp <adhoc607gmail.com>
2026-02-09 01:19:04 -0500

    deploy site config 2

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,11 +13,11 @@ export function renderWithTemplate(template, parentElement, data, callback) {
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../src/partials/header.html");
+  const headerTemplate = await loadTemplate("/partials/header.html");
   const headerElement = document.querySelector("#main-header");
   renderWithTemplate(headerTemplate, headerElement);
 
-  const footerTemplate = await loadTemplate("../src/partials/footer.html");
+  const footerTemplate = await loadTemplate("/partials/footer.html");
   const footerElement = document.querySelector("#main-footer");
   renderWithTemplate(footerTemplate, footerElement);
 }
