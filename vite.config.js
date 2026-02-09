208c26a79b6254fabdda6288dda2561b927ca1c4
dereklp <adhoc607gmail.com>
2026-02-09 02:15:08 -0500

    deploy site config 4

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        bible: resolve(__dirname, 'src/hero_page/hero_page.html'),
-        heroes: resolve(__dirname, 'src/verse_page/verse_page.html'),
+        bible: resolve(__dirname, 'hero_page/hero_page.html'),
+        heroes: resolve(__dirname, 'verse_page/verse_page.html'),
         header: resolve(__dirname, 'public/partials/header.html'),
         footer: resolve(__dirname, 'public/partials/footer.html')
       }
